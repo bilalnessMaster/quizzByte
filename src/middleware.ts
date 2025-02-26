b1f1f593a41aa8a52bb3757f38dbd39281a3b08b
bilalnessMaster <jijahgiga@gmail.com>
2025-02-26 13:17:46 +0000

    add the unstable_allow

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,4 +5,10 @@ export {auth as middleware} from '@/auth'
 export const config = {
     runtime : "nodejs",
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+    unstable_allowDynamic: [
+        // allows a single file
+        "/src/db/lib/dbConnect.js",
+        // use a glob to allow anything in the function-bind 3rd party module
+        "/node_modules/mongoose/dist/**",
+    ],
 }
