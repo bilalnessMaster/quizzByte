bf14f5c8541eab8bbc3f6f8e46fcb0b5a74c0549
bilalnessMaster <jijahgiga@gmail.com>
2025-02-27 17:47:41 +0000

    update to nextjs 15.2 so now we can use the node runtime on the middleware

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        nodeMiddleware: true, // Enable Node.js runtime in Middleware
+},
   eslint : {
     ignoreDuringBuilds : true
   }
