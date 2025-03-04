ae2b40ceb1e4f5d8bfe24e657bab11b6042844e5
bilalnessMaster <jijahgiga@gmail.com>
2025-03-04 12:11:19 +0000

    fix hydration error on sign in page cause : video component

--- src/app/auth/component/Background.tsx
+++ src/app/auth/component/Background.tsx
@@ -1,7 +1,16 @@
 ''
 
-const Background = () => {
+import { useEffect, useState } from "react";
 
+const Background = () => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true)
+    }, []);
+    if(!isLoading) return <div className="h-full w-full flex justify-center items-center">
+         <div className="border-2 border-t-2 border-t-white size-12 flex animate-spin rounded-full"/>;
+    </div>
     return (
         <div className="flex-1 p-2 h-screen overflow-hidden relative  ">
 
@@ -11,17 +20,19 @@ const Background = () => {
               <h1 className="text-8xl max-w-2xl font-medium font-bricolage px-2 text-neutral-100">Digital platform for creating quizzes and QCMs.</h1>
             </div>
             <div className="h-full rounded-lg">
+
             <video
-             src="https://res.cloudinary.com/dpwnw6ltb/video/upload/v1740683803/wirntpv6c0arc7pvyoge.mp4"
-              width="320" height="240"
+              src="https://res.cloudinary.com/dpwnw6ltb/video/upload/v1740683803/wirntpv6c0arc7pvyoge.mp4"
+              width="320"
+              height="240"
               loop
               muted
               autoPlay
               className="h-full w-full object-cover rounded-2xl"
-
             >
-               <source src="/public/background.mp4" type="video/mp4" />
+              <source src="/public/background.mp4" type="video/mp4" />
             </video>
+
 
             </div>
           </div>
